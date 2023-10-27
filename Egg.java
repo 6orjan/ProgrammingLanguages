b5448024056de411ae1f37797c2a171d3f983bf1
Borjan Georgievski <borjang6@gmail.com>
2023-10-27 18:18:36 +0200

    Update Egg.java

--- Egg.java
+++ Egg.java
@@ -3,24 +3,34 @@ package verificationTest;
 public class classVExericseII {
 
 	public static class Egg{
-		public void Egg() {
+		public void printEgg() {
 			System.out.println("Each egg has an albumen and yolk");
+			int category;
 		}
 		public void print() {
 			System.out.println("An egg is a nutritional product");
 		}
+		int category;
+		public Egg (int c) {
+			category = c;
+		}
 	}
 	public static class ostrich_egg extends Egg{
+		public ostrich_egg(int c) {
+			super(c);
+			System.out.println("A constructor of ostrich_egg that utilizes the constructor of egg");
+		}
+
 		public void Egg() {
 			System.out.println("The ostrich egg has the largest albumen and yolk");
 		}
 	}
 	public static void main(String[] args) {
-		Egg henegg = new Egg();
-		Egg ostrichegg = new ostrich_egg();
-		henegg.Egg();
+		Egg henegg = new Egg(1);
+		Egg ostrichegg = new ostrich_egg(1);
+		henegg.printEgg();
 		henegg.print();
-		ostrichegg.Egg();
+		ostrichegg.printEgg();
 	}
 
 }
