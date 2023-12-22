2c74507c678ab180013fa1b5c3a2ee64a431f025
Borjan Georgievski <borjang6@gmail.com>
2023-12-22 20:43:05 +0100

    Update TwoDimensionalException.java

--- TwoDimensionalException.java
+++ TwoDimensionalException.java
@@ -1,49 +1,42 @@
 import java.util.Scanner;
 
-
 public class TwoDimension {
 
-	public static void main(String[] args) {
-		 Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+    	 Scanner scanner = new Scanner(System.in);
 
-	       
-	        System.out.print("Enter number of rows");
-	        int rows = scanner.nextInt();
+         System.out.print("Enter number of rows: ");
+         int rows = scanner.nextInt();
 
-	        System.out.print("Enter number of columns");
-	        int columns = scanner.nextInt();
+         System.out.print("Enter number of columns: ");
+         int columns = scanner.nextInt();
 
-	        
-	        int[][] array = new int[rows][columns];
+         int[][] array = new int[rows][columns];
+         int sum = 0;
 
-	      
-	        int sum = 0;
+         for (int i = 0; i < rows; i++) {
+             for (int j = 0; j < columns; j++) {
+                 boolean validInput = false;
 
-	        try {
-	            
+                 while (!validInput) {
+                     try {
+                         System.out.print("Enter element at [" + i + "][" + j + "]: ");
+                         array[i][j] = scanner.nextInt();
+                         sum += array[i][j];
+                         validInput = true;
+                     } catch (Exception e) {
+                         System.out.println("Error, please enter a number");
+                         scanner.nextLine();  
+                     }
+                 }
+             }
+         }
 
-	            for (int i = 0; i < rows; i++) {
-	                for (int j = 0; j < columns; j++) {
-	                    System.out.print("Enter element at  [" + i + "][" + j + "]: ");
-	                    array[i][j] = scanner.nextInt();
-	                    sum += array[i][j];
-	                }
-	            }
+         double average = (double) sum / (rows * columns);
 
-	         
-	            double average = (double) sum / (rows * columns);
+         System.out.println("The sum of the elements is " + sum);
+         System.out.println("The average of the elements is " + average);
 
-	            
-	            System.out.println("The sum of the elements is " + sum);
-	            System.out.println("The average of the elements is " + average);
-
-	        } catch (Exception e) {
-	            System.out.println("Error");
-	        } finally {
-	          
-	            scanner.close();
-	        }
-
-	}
-
+         scanner.close();
+    }
 }
